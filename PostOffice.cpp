a7a89a7321f21e3a625ddaa25b8d9c8dc88bbf2c
Samuel Bohovic <xbohov01@stud.fit.vutbr.cz>
2018-11-18 11:39:35 +0100

    added arg processing

--- PostOffice.cpp
+++ PostOffice.cpp
@@ -3,3 +3,52 @@
 //Samuel Bohovic xbohov01
 //Danil Grigorev xgrigoxx <<<<<<<<<<<<<<< TODO TVOJE ID
 
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    //Argument processing
+    int c;
+    bool laborDivision = false;
+    int machines = 0;
+    int simpleClerks = 0;
+    int incomingClerks = 0;
+    int outgoingClerks = 0;
+    int otherClerks = 0;
+
+    if (argc < 5){
+        fprintf(stderr, "Not enough arguments, consult README for correct usage\n");
+        return 1;
+    }
+
+    while ((c = getopt (argc, argv, "c:m:i:o:")) != -1){
+        switch (c) {
+            case 'c':
+                laborDivision = false;
+                simpleClerks = atoi(optarg);
+                break;
+            case 'm':
+                machines = atoi(optarg);
+                break;
+            case 'i':
+                laborDivision = true;
+                incomingClerks = atoi(optarg);
+                break;
+            case 'o':
+                laborDivision = true;
+                outgoingClerks = atoi(optarg);
+                break;
+            case '?':
+                fprintf(stderr, "Incorrect arguments, consult README for correct usage\n");
+                return 1;
+            default:
+                fprintf(stderr, "Argument error\n");
+                return 1;
+        }
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -6,11 +6,13 @@ It models 8 hour work day at a post office.
 Prototype argument structure
 3 modes of operation
 1. all clerks do all jobs
-    ./postoffice -u [number of clerks]
+    ./postoffice -c [number of clerks] -m [machines]
 
 2. one group of clerks hadles incoming another outgoing mail
-    ./postoffice -i [no clerks for incoming] -o [no clerks for outgoing]
+    ./postoffice -i [no clerks for incoming] -o [no clerks for outgoing] -m [machines]
 
+
+////DEPRECATED
 3. one group for incoming letters, outgoing letters, incoming parcels, outgoing parcels
     ./postoffice -iL [no clerks for incoming letters] -oL [no clerks for outgoing letters] -iP [no clerks for incoming parcels] -oP [no clerks for outgoing parcels]
 
